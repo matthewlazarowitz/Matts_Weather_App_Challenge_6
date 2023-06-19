013b1ea13795f2f14a5ecd52ff94d628e54810ef
Laz <nyys811@aol.com>
2023-06-19 00:08:22 -0400

    added function that gets the forecast info and loops it for each day then creates HTML content for each day

--- script.js
+++ script.js
@@ -20,6 +20,53 @@ $(function () {
         cityInput.val("");
 
         addCityToSearchHistory(city);
-
     });
+
+    function getCurrentWeather(city) {
+        var currentWeatherURL =
+            "https://api.openweathermap.org/data/2.5/weather?q=" +
+            city +
+            "&appid=" +
+            apiKey +
+            "&units=imperial";
+
+        $.ajax({
+            url: currentWeatherURL,
+            method: "GET",
+        }).then(function (response) {
+            
+            var cityName = response.name;
+            var date = new Date(response.dt * 1000); 
+            var icon = response.weather[0].icon;
+            var temperature = response.main.temp;
+            var humidity = response.main.humidity;
+            var windSpeed = response.wind.speed;
+
+            
+            var currentWeatherContent =
+                "<h2>" +
+                cityName +
+                "</h2>" +
+                "<p>Date: " +
+                date.toLocaleDateString() +
+                "</p>" +
+                "<img src='https://openweathermap.org/img/w/" +
+                icon +
+                ".png' alt='Weather Icon'>" +
+                "<p>Temperature: " +
+                temperature +
+                " &#8457;</p>" +
+                "<p>Humidity: " +
+                humidity +
+                "%</p>" +
+                "<p>Wind Speed: " +
+                windSpeed +
+                " mph</p>";
+
+            
+            currentWeather.html(currentWeatherContent);
+        });
+    }
+    
+
 });
