5ea9b9972d86d9fe2a4091a9e074cfabf23efb3c
Laz <nyys811@aol.com>
2023-06-18 23:47:03 -0400

    added jquery wrap and defined variables

---
+++ script.js
@@ -0,0 +1,9 @@
+$(function () {
+    var apiKey = "ae5b0dcead613ff683cbd248a8ae7a91";
+    var searchForm = $("#search-form");
+    var cityInput = $("#city-input");
+    var currentWeather = $("#current-weather");
+    var forecast = $("#forecast");
+    var searchHistory = $("#search-history-buttons");
+
+});
